85d8cac504208ac8d7884883224f901df90dbc02
Anais Cristina Aponte Castillo <anaisaponte@gmail.com>
2026-01-17 10:40:28 +0000

    ultimos ejercicios

--- exercises/038-sort-tuples-ascending/app.py
+++ exercises/038-sort-tuples-ascending/app.py
@@ -1,3 +1,10 @@
 from operator import itemgetter
-
 # Your code here
+
+# def sort_tuples_ascending(lista):
+#     sorted(lista,key=itemgetter(0))
+#     resultado = tuple(lista.split(",") for item in lista)
+        
+        
+# sort_tuples_ascending(['Tom,19,80', 'John,20,90', 'Jony,17,91', 'Jony,17,93', 'Jason,21,85'])
+
--- exercises/039-class-that-iterates/app.py
+++ exercises/039-class-that-iterates/app.py
@@ -1,1 +1,10 @@
 # Your code here
+def divisibles_7(n):
+    for i in range(n):
+        if (i % 7 == 0):
+            yield i
+
+for i in divisibles_7(15):
+    print(i)
+
+    
